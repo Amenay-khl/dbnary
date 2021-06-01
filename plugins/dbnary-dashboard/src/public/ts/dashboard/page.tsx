af97bd388c7b76f8f90d2a98df6ed42f45b3509a
Gilles Sérasset <gilles.serasset@imag.fr>
2021-06-01 11:41:50 +0200

    fix(typescript): updtae main bar chart with provider

--- plugins/dbnary-dashboard/src/public/ts/dashboard/page.tsx
+++ plugins/dbnary-dashboard/src/public/ts/dashboard/page.tsx
@@ -2,13 +2,14 @@ import React, { FC } from "react";
 import { MainBarChart } from "./mainBarChart";
 import { StatsLine } from "./statsLine";
 import { decorations } from "./styles";
+import { doMainCountsForAllLanguages } from "../wp-api/sparql.get";
 
 /* istanbul ignore next: Example implementations gets deleted the most time after plugin creation! */
 const DbnaryDashboard: FC<{}> = () => {
     return (
         <div className="wp-styleguide">
             <StatsLine decorations={decorations} />
-            <MainBarChart decorations={decorations} />
+            <MainBarChart decorations={decorations} provider={doMainCountsForAllLanguages} />
         </div>
     );
 };
