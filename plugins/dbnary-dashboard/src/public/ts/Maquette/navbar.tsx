87f7d3e3ffa64a96bf16f3227d5ac98a87e50741
@verwaera <verwaerde.arthur@gmail.com>
2021-07-02 18:59:43 +0200

    Navbar basique

--- plugins/dbnary-dashboard/src/public/ts/Maquette/navbar.tsx
+++ plugins/dbnary-dashboard/src/public/ts/Maquette/navbar.tsx
@@ -1,22 +1,42 @@
+import { colors, Grid, makeStyles } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
-import "./navbar.css";
 
 export default function Navbar() {
     return (
         <div>
-            <div className="contBtn">
-                <div className="Onglet">General</div>
-                <div className="Onglet">fra</div>
-                <div className="Onglet">eng</div>
-                <div className="Onglet">deu</div>
-                <div className="Onglet">ell</div>
-                <div className="Onglet">fin</div>
-                <div className="Onglet">jpn</div>
-                <div className="Onglet">ita</div>
-                <div className="Onglet">por</div>
-                <div className="Onglet">rus</div>
-                <div className="Onglet">tur</div>
-            </div>
+            <Grid container item spacing={1} justify="space-between" alignItems="center">
+                <div>General</div>
+                <Grid item>
+                    <div>fra</div>
+                </Grid>
+                <Grid item>
+                    <div>eng</div>
+                </Grid>
+                <Grid item>
+                    <div>deu</div>
+                </Grid>
+                <Grid item>
+                    <div>ell</div>
+                </Grid>
+                <Grid item>
+                    <div>fin</div>
+                </Grid>
+                <Grid item>
+                    <div>jpn</div>
+                </Grid>
+                <Grid item>
+                    <div>ita</div>
+                </Grid>
+                <Grid item>
+                    <div>por</div>
+                </Grid>
+                <Grid item>
+                    <div>rus</div>
+                </Grid>
+                <Grid item>
+                    <div>tur</div>
+                </Grid>
+            </Grid>
         </div>
     );
 }
