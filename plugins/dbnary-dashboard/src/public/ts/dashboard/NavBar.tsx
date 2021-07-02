374c6a922f9ba20ee72a36fab581a972dde0c8d8
l3miage-khiala <63787082+l3miage-khiala@users.noreply.github.com>
2021-07-02 15:05:50 +0200

    fix error

---
+++ plugins/dbnary-dashboard/src/public/ts/dashboard/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+// import Logo from "../../assets/images/logo.svg"
+
+export default function Navbar(){
+  return (
+<div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+    
+    )
+}
