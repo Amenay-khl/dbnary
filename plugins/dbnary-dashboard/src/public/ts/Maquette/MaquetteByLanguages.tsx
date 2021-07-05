617c9a458c8351a33d6dac6df380aedafcb72926
l3miage-khiala <63787082+l3miage-khiala@users.noreply.github.com>
2021-07-05 17:12:08 +0200

    realisation du grid au lieu du tableau pour le dashboard

--- plugins/dbnary-dashboard/src/public/ts/Maquette/MaquetteByLanguages.tsx
+++ plugins/dbnary-dashboard/src/public/ts/Maquette/MaquetteByLanguages.tsx
@@ -4,72 +4,15 @@ import { NumberOfLexicalRelationsForFr } from "../numberOfLexicalRelationsByLang
 import { NumberOfTranslationsForFr } from "../numberOfTranslationsByLanguages/numberOfTranslationsForFr/";
 
 import Grid from "@material-ui/core/Grid";
-import { StatCard, MainCountStats } from "./cards/statCard";
 
-import { SparqlResponse, doMainCountsRestCall } from "../wp-api/sparql.get";
 import { DecorationSpec } from "./styles";
-import { colors, makeStyles } from "@material-ui/core";
 
 /* The decorations to provide to the generic barchart */
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        height: "100%"
-    },
-    avatar: (props: DecorationSpec) => ({
-        backgroundColor: props.color,
-        height: 38,
-        width: 38
-    }),
-    positiveDifferenceIcon: {
-        color: colors.green[900]
-    },
-    negativeDifferenceIcon: {
-        color: colors.red[900]
-    },
-    positiveDifferenceValue: {
-        color: colors.green[900],
-        marginRight: theme.spacing(1)
-    },
-    negativeDifferenceValue: {
-        color: colors.red[900],
-        marginRight: theme.spacing(1)
-    },
-    smallPadding: {
-        padding: theme.spacing(1),
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(1),
-        "&:last-child": {
-            paddingBottom: theme.spacing(1)
-        }
-    },
-    greyBackground: {
-        backgroundColor: colors.grey[100]
-    }
-}));
 
 const MaquetteByLanguages: FC<{ decorations: Record<string, DecorationSpec> }> = ({ decorations, ...rest }) => {
     // const [openTab, setOpenTab] = useState(1);
 
     return (
-        // <div>
-        //     <table>
-        //         <tbody>
-        //             <tr>
-        //                 <td>
-        //                     <NumberOfElementsForFr />
-        //                 </td>
-        //                 <td>
-        //                     <NumberOfLexicalRelationsForFr />
-        //                 </td>
-        //                 <td>
-        //                     <NumberOfTranslationsForFr />
-        //                 </td>
-        //             </tr>
-        //         </tbody>
-        //     </table>
-        // </div>
-
         <Grid container item xs={12} spacing={3} justify="space-between" alignItems="center" {...rest}>
             <Grid item xs={4}>
                 <NumberOfElementsForFr />{" "}
--- plugins/dbnary-dashboard/src/public/ts/Maquette/mainBarChart.tsx
+++ plugins/dbnary-dashboard/src/public/ts/Maquette/mainBarChart.tsx
@@ -2,41 +2,33 @@ import React, { Component, FC, useEffect, useState } from "react";
 import { NumberOfElements } from "../numberOfElements";
 import { NumberOfLexicalRelations } from "../numberOfLexicalRelations/";
 import { NumberOfTranslations } from "../numberOfTranslations";
+import { DecorationSpec } from "./styles";
+import Grid from "@material-ui/core/Grid";
 
 /* The decorations to provide to the generic barchart */
 
-const MainBarChart: FC<any> = () => {
-    const [openTab, setOpenTab] = useState(1);
-
+const MainBarChart: FC<{ decorations: Record<string, DecorationSpec> }> = ({ decorations, ...rest }) => {
     return (
-        <div>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <NumberOfElements />
-                        </td>
-                        <td>
-                            <NumberOfLexicalRelations />
-                        </td>
-                        <td>
-                            <NumberOfTranslations />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <NumberOfElements />
-                        </td>
-                        <td>
-                            <NumberOfElements />
-                        </td>
-                        <td>
-                            <NumberOfElements />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <Grid container item xs={12} spacing={3} justify="space-between" alignItems="center" {...rest}>
+            <Grid item xs={4}>
+                <NumberOfElements />{" "}
+            </Grid>
+            <Grid item xs={4}>
+                <NumberOfLexicalRelations />
+            </Grid>
+            <Grid item xs={4}>
+                <NumberOfTranslations />
+            </Grid>
+            <Grid item xs={4}>
+                <NumberOfElements />
+            </Grid>
+            <Grid item xs={4}>
+                <NumberOfElements />
+            </Grid>
+            <Grid item xs={4}>
+                <NumberOfElements />
+            </Grid>
+        </Grid>
     );
 };
 
